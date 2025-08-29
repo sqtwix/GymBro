1a0763713260d5f8d24d32e3ca122ac5a88200cf
sqtwix <ivan20140767@gmail.com>
2025-08-29 23:16:20 +0300

    Creation register method in controller

---
+++ GemBro.API/GemBro.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GymBro.API.Data;
+
+namespace GymBro.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController:ControllerBase
+    {
+        private readonly ApplicationDbContext _context; 
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+    }
+}
